cc67fd3beeba374cdea1bc1b360182814730f465
NETNEO <rival.moro@hotmail.it>
2024-08-01 11:12:49 +0200

    added files .gitignore .env db.js, data base connected width mongoose and server connected with express

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("desde node.js");
+import express from "express";
+import dotenv from "dotenv";
+import conectarDB from "./config/db.js";
+
+const app = express();
+dotenv.config();
+conectarDB();
+
+app.use("/", (req, res) => {
+  res.send("Hola Mario Rivera");
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`servidor funcionando eln el servidor ${PORT}`);
+});
