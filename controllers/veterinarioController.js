3c29de4459beb1be9573f00a5de23932a49d7953
NETNEO <rival.moro@hotmail.it>
2024-08-01 15:35:42 +0200

    controllers for routes and adding a new veterinario at the database MONGODB

---
+++ controllers/veterinarioController.js
@@ -0,0 +1,17 @@
+import Veterinario from "../models/Veterinario.js";
+
+const registrar = async (req, res) => {
+  try {
+    const veterinario = new Veterinario(req.body);
+    const veterinarioGuardado = await veterinario.save();
+    res.json(veterinarioGuardado);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const perfil = (req, res) => {
+  res.json({ msg: "mostrando perfil" });
+};
+
+export { registrar, perfil };
--- routes/veterinarioRoutes.js
+++ routes/veterinarioRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
+import { registrar, perfil } from "../controllers/veterinarioController.js";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("desde API/veternarios");
-});
+router.post("/", registrar);
+router.get("/perfil", perfil);
 
 export default router;
